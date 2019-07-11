96dc327c639a4677d301fa268657ef4ce181b3c9
Alex Len <alexlen01@users.noreply.github.com>
2019-07-10 23:49:01 -0400

    Added samples of different uses of Spring beans

--- src/test/java/com/oreilly/demo/DemoApplicationTests.java
+++ src/test/java/com/oreilly/demo/DemoApplicationTests.java
@@ -2,15 +2,44 @@ package com.oreilly.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.text.NumberFormat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
+    @Autowired
+    private ApplicationContext ctx;
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    @Qualifier("defaultCurrencyFormat")
+    private NumberFormat nf;
+
+    @Test
+    public void defaultCurrency() {
+        double amount = 12345623.90101;
+        System.out.println(nf.format(amount));
+    }
+
+    @Test
+    public void germanCurrency() {
+        double amount = 12345623.90101;
+        NumberFormat dnf = ctx.getBean("germanCurrencyFormat", NumberFormat.class);
+        System.out.println(dnf.format(amount));
+    }
+
+    @Test
+    public void contextLoads() {
+        int count = ctx.getBeanDefinitionCount();
+        System.out.println("There are " + count + " beans in the application context");
+        for (String name : ctx.getBeanDefinitionNames()) {
+            System.out.println("Bean: " + name);
+        }
+    }
 
 }
